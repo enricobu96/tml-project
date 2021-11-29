c868f3c9c6cbe1256d31dfdac8b40489d0febd44
enricobu96 <enrico.buratto.1@studenti.unipd.it>
2021-11-29 15:51:14 +0200

    fix stop_time category

--- preprocess.py
+++ preprocess.py
@@ -6,13 +6,13 @@ from datetime import datetime
 from sklearn.model_selection import  train_test_split
 from sklearn.linear_model import LogisticRegression
 pd.set_option('display.max_columns', None)
+_night = ['22:', '23:', '00:', '01:', '02:', '03:', '04:', '05:', '06:', '07:']
 
 data = pd.read_csv('data/police_project.csv')
 data = data.drop(['stop_date','county_name', 'driver_age_raw', 'violation_raw', 'stop_outcome', 'drugs_related_stop', 'search_type'], axis=1)
 data = data.dropna()
-data = data.drop(['stop_time'], axis=1)
-#data['stop_time'] = data['stop_time'].apply(lambda x: 0 if (datetime.strptime(x, '%H:%M') > datetime(year=1, month=1, day=1, hour=22, minute=00) and datetime.strptime(x, '%H:%M') <= datetime(year=3000, day=31, month=12, hour=7, minute=00)) else 1)
-#print(data['stop_time'].unique())
+
+data['stop_time'] = data['stop_time'].apply(lambda x: 0 if any(n in x for n in _night) else 1)
 
 data['driver_gender'] = data['driver_gender'].astype('category').cat.codes
 data['driver_race'] = data['driver_race'].astype('category').cat.codes
@@ -20,7 +20,7 @@ data['violation'] = data['violation'].astype('category').cat.codes
 data['search_conducted'] = data['search_conducted'].astype('category').cat.codes
 data['is_arrested'] = data['is_arrested'].astype('category').cat.codes
 data['stop_duration'] = data['stop_duration'].astype('category').cat.codes
-#print(data.shape)
+# #print(data.shape)
 
 X = data[data.columns.difference(['is_arrested'])]
 y = data['is_arrested']
