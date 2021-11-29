02636474b3f1256251123a374c8a7b0d47799b93
basselslim <bassel.slim99@gmail.com>
2021-11-29 14:12:58 +0200

    preprocessed data

--- preprocess.py
+++ preprocess.py
@@ -3,13 +3,30 @@ import numpy.random as npr
 import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
-
+from sklearn.model_selection import  train_test_split
+from sklearn.linear_model import LogisticRegression
 pd.set_option('display.max_columns', None)
 
 data = pd.read_csv('data/police_project.csv')
-data = data.drop(['stop_date','county_name', 'driver_age_raw', 'violation_raw', 'stop_outcome', 'drugs_related_stop'], axis=1)
+data = data.drop(['stop_date','county_name', 'driver_age_raw', 'violation_raw', 'stop_outcome', 'drugs_related_stop', 'search_type'], axis=1)
+data = data.dropna()
+data = data.drop(['stop_time'], axis=1)
 #data['stop_time'] = data['stop_time'].apply(lambda x: 0 if (datetime.strptime(x, '%H:%M') > datetime(year=1, month=1, day=1, hour=22, minute=00) and datetime.strptime(x, '%H:%M') <= datetime(year=3000, day=31, month=12, hour=7, minute=00)) else 1)
 #print(data['stop_time'].unique())
 
-data['driver_gender'] = data['driver_gender'].apply(lambda x: 0 if x == 'M' else 1)
-data['driver_age'] = data['driver_age'].apply(lambda x: print(type(x)))
+data['driver_gender'] = data['driver_gender'].astype('category').cat.codes
+data['driver_race'] = data['driver_race'].astype('category').cat.codes
+data['violation'] = data['violation'].astype('category').cat.codes
+data['search_conducted'] = data['search_conducted'].astype('category').cat.codes
+data['is_arrested'] = data['is_arrested'].astype('category').cat.codes
+data['stop_duration'] = data['stop_duration'].astype('category').cat.codes
+#print(data.shape)
+
+X = data[data.columns.difference(['is_arrested'])]
+y = data['is_arrested']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = LogisticRegression().fit(X_train, y_train)
+print(clf.score(X_test, y_test))
+
+
